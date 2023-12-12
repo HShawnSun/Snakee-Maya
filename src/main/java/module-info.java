b6c2ed365df5824716e5ae50a5c44fe799a99558
He SUN <scyhs4@exmail.nottingham.ac.uk>
2023-12-12 16:20:13 +0000

    adjust img feed structure

---
+++ src/main/java/module-info.java
@@ -0,0 +1,22 @@
+module com.example.comp2013sunhe {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires eu.hansolo.fx.countries;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+    requires java.datatransfer;
+    requires java.desktop;
+    requires javafx.media;
+    requires javafx.swing;
+    requires javafx.graphics;
+    requires javafx.base;
+
+    exports com.java.comp2013sunhe to javafx.graphics;
+}
